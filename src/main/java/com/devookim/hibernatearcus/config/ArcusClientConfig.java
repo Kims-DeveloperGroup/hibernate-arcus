3acb02b878ffa048f1685c60de12dafe0b9d3f67
rica.rdo <rica.rdo@kakaopaycorp.com>
2021-03-12 22:14:58 +0900

    Static import HibernateArcusProperties in ArcusClientConfig

--- src/main/java/com/devookim/hibernatearcus/config/ArcusClientConfig.java
+++ src/main/java/com/devookim/hibernatearcus/config/ArcusClientConfig.java
@@ -7,31 +7,36 @@ import net.spy.memcached.ConnectionFactoryBuilder;
 
 import java.util.Map;
 
+import static com.devookim.hibernatearcus.config.HibernateArcusProperties.*;
+
 @Slf4j
 public class ArcusClientConfig {
     public final boolean fallbackEnabled;
     public final boolean initFallbackMode;
     public final int healthCheckIntervalInSec;
+    public final int domainDataTTL;
     private final int poolSize;
     private final Map<String, String> properties;
     private final String host;
     private final String serviceCode;
 
     public ArcusClientConfig(Map<String, String> properties) {
-        this.host = properties.getOrDefault(HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_HOST, "localhost:2181");
-        this.serviceCode = properties.getOrDefault(HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_SERVICE_CODE, "");
-        this.poolSize = Integer.parseInt(properties.getOrDefault(HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_POOL_SIZE, "1"));
-        this.fallbackEnabled = Boolean.parseBoolean(properties.getOrDefault(HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_FALLBACK_ENABLED, "true"));
-        this.initFallbackMode = Boolean.parseBoolean(properties.getOrDefault(HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_INIT_FALLBACK_MODE, "false"));
-        this.healthCheckIntervalInSec = Integer.parseInt(properties.getOrDefault(HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_HEALTH_CHECK_INTERVAL_IN_SEC, "10"));
+        this.host = properties.getOrDefault(HIBERNATE_CACHE_ARCUS_HOST, "localhost:2181");
+        this.serviceCode = properties.getOrDefault(HIBERNATE_CACHE_ARCUS_SERVICE_CODE, "");
+        this.poolSize = Integer.parseInt(properties.getOrDefault(HIBERNATE_CACHE_ARCUS_POOL_SIZE, "1"));
+        this.domainDataTTL = Integer.parseInt(properties.getOrDefault(HIBERNATE_CACHE_ARCUS_DOMAIN_DATA_TTL, "0"));
+
+        this.fallbackEnabled = Boolean.parseBoolean(properties.getOrDefault(HIBERNATE_CACHE_ARCUS_FALLBACK_ENABLED, "true"));
+        this.initFallbackMode = Boolean.parseBoolean(properties.getOrDefault(HIBERNATE_CACHE_ARCUS_INIT_FALLBACK_MODE, "false"));
+        this.healthCheckIntervalInSec = Integer.parseInt(properties.getOrDefault(HIBERNATE_CACHE_ARCUS_HEALTH_CHECK_INTERVAL_IN_SEC, "10"));
         this.properties = properties;
     }
 
     public ArcusClientPool createArcusClientPool() {
         log.info("Creating arcus client pool");
         ConnectionFactoryBuilder cfb = new ConnectionFactoryBuilder();
-        cfb.setMaxReconnectDelay(Long.parseLong(properties.getOrDefault(HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_RECONNECT_INTERVAL_IN_SEC, "10000")));
-        cfb.setOpTimeout(Long.parseLong(properties.getOrDefault(HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_OP_TIMEOUT, "10000")));
+        cfb.setMaxReconnectDelay(Long.parseLong(properties.getOrDefault(HIBERNATE_CACHE_ARCUS_RECONNECT_INTERVAL_IN_SEC, "10000")));
+        cfb.setOpTimeout(Long.parseLong(properties.getOrDefault(HIBERNATE_CACHE_ARCUS_OP_TIMEOUT, "10000")));
         return ArcusClient.createArcusClientPool(
                 host,
                 serviceCode,
