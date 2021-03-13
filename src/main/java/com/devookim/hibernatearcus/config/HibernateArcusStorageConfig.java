c488813406a53f139c1bb6b8c3c6df6722a072aa
devooKim <devookim@gmail.com>
2021-03-13 20:32:35 +0900

    [Issues/11] TTL of DomainData EntityCaching is configurable (#12)
    * Version Up to 1.2.0-RELEASE
    
    * Static import HibernateArcusProperties in ArcusClientConfig
    
    * Add HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_DOMAIN_DATA_TTL
    
    * Remove ArcusClientConfig.domainDataTTL
    
    * Add ttl to DomainDataHibernateArcusStorageAccess
    
    * Modify value of HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_DOMAIN_DATA_TTL
    
    * Version 1.2.1-SNAPSHOT
    
    Co-authored-by: rica.rdo <rica.rdo@kakaopaycorp.com>

--- src/main/java/com/devookim/hibernatearcus/config/HibernateArcusStorageConfig.java
+++ src/main/java/com/devookim/hibernatearcus/config/HibernateArcusStorageConfig.java
@@ -4,12 +4,17 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Map;
 
+import static com.devookim.hibernatearcus.config.HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_DOMAIN_DATA_TTL;
+import static com.devookim.hibernatearcus.config.HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_EVICTION_REGION_GROUP_ON_CACHE_UPDATE;
+
 public class HibernateArcusStorageConfig {
 
     public final HashSet<String> evictionRegionGroupOnCacheUpdate;
+    public final int domainDataTTL;
 
     public HibernateArcusStorageConfig(Map<String, String> properties) {
         evictionRegionGroupOnCacheUpdate = new HashSet<>();
-        Collections.addAll(evictionRegionGroupOnCacheUpdate, properties.getOrDefault(HibernateArcusProperties.HIBERNATE_CACHE_ARCUS_EVICTION_REGION_GROUP_ON_CACHE_UPDATE, "").split(","));
+        domainDataTTL = Integer.parseInt(properties.getOrDefault(HIBERNATE_CACHE_ARCUS_DOMAIN_DATA_TTL, "0"));
+        Collections.addAll(evictionRegionGroupOnCacheUpdate, properties.getOrDefault(HIBERNATE_CACHE_ARCUS_EVICTION_REGION_GROUP_ON_CACHE_UPDATE, "").split(","));
     }
 }
