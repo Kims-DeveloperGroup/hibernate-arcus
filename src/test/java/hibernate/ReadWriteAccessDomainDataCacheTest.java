74e9ceb5a6f23d2bc0409569fdfb6b732d5ec87e
rica.rdo <rica.rdo@kakaopaycorp.com>
2021-04-16 18:52:26 +0900

    Add multiple transaction test

--- src/test/java/hibernate/ReadWriteAccessDomainDataCacheTest.java
+++ src/test/java/hibernate/ReadWriteAccessDomainDataCacheTest.java
@@ -3,6 +3,7 @@ package hibernate;
 import com.devookim.hibernatearcus.factory.HibernateArcusRegionFactory;
 import hibernate.domain.ReadWriteAccessDomainData;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
@@ -13,6 +14,12 @@ import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,7 +39,8 @@ public class ReadWriteAccessDomainDataCacheTest extends BaseCoreFunctionalTestCa
         cfg.setProperty(Environment.URL, "jdbc:h2:mem:db1;DB_CLOSE_DELAY=-1;");
         cfg.setProperty(Environment.GENERATE_STATISTICS, "true");
         cfg.setProperty(Environment.USE_SECOND_LEVEL_CACHE, "true");
-        cfg.setProperty(Environment.USE_QUERY_CACHE, "true");
+        cfg.setProperty(Environment.USE_QUERY_CACHE, "false");
+        cfg.setProperty(Environment.SHOW_SQL, "true");
         cfg.setProperty(Environment.CACHE_REGION_FACTORY, HibernateArcusRegionFactory.class.getName());
     }
 
@@ -230,6 +238,106 @@ public class ReadWriteAccessDomainDataCacheTest extends BaseCoreFunctionalTestCa
         s.getTransaction().commit();
         s.close();
         stats.logSummary();
+    }
+
+    @Test
+    public void test_whenMultipleTransactionUpdatesAtTheSameTime_thenCacheItemAndDbEntityAreIdentical() {
+        Statistics stats = sessionFactory().getStatistics();
+        Long id = null;
+
+        // 데이터를 인서트하고 캐쉬에 넣는다
+        Session s = openSession();
+        s.beginTransaction();
+        ReadWriteAccessDomainData readWriteAccessDomainData = new ReadWriteAccessDomainData(System.currentTimeMillis(), "0");
+        id = (Long) s.save(readWriteAccessDomainData);
+        s.flush();
+        s.getTransaction().commit();
+        s.close();
+
+        int parallel = 20;
+        // 이름과 값을 다른 트랜잭션 에서 동시에 실행한다
+        try {
+            updateNameAndValue(id, parallel);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+
+        //캐쉬에서 데이터를 가져온다
+        s = openSession();
+        s.beginTransaction();
+        ReadWriteAccessDomainData fromCache = s.get(ReadWriteAccessDomainData.class, id);
+        s.getTransaction().commit();
+        s.close();
+
+
+        //디비에서 데이터를 가져온다.
+        s = openSession();
+        s.beginTransaction();
+        Query query = s.getNamedQuery("domainDataNamedQueryById");
+        query.setParameter("id", id);
+        log.info("fromDB");
+        ReadWriteAccessDomainData fromDB = (ReadWriteAccessDomainData) query.uniqueResult();
+        log.info("fromDB");
+        s.getTransaction().commit();
+        s.close();
+
+        // 캐쉬에서
+        // value 0 -> 1 확인한다
+        // 이름이   updated로 바뀌었는지 확인하다
+        assertThat(fromCache.value).isEqualTo(1);
+        assertThat(fromCache.getName()).isEqualTo("updated");
+
+        // 디비랑 캐쉬랑 값이 같은지 확인한다
+        assertThat(fromCache.value).isEqualTo(fromDB.getValue());
+        assertThat(fromCache.getName()).isEqualTo(fromDB.getName());
+    }
+
+    private void updateNameAndValue(Long id, int parallel) throws InterruptedException {
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(parallel);
+
+        Callable<ReadWriteAccessDomainData> incrementDataValue = () -> {
+            Session s;
+            ReadWriteAccessDomainData data;
+            s = openSession();
+            s.beginTransaction();
+            data = s.get(ReadWriteAccessDomainData.class, id);
+            log.info("===1 name: {} value: {}", data.getName(), data.value);
+            data.value += 1;
+            s.flush();
+            s.getTransaction().commit();
+            s.clear();
+            s.close();
+            return data;
+        };
+
+        Callable<ReadWriteAccessDomainData> setName = () -> {
+            Session s;
+            ReadWriteAccessDomainData data;
+            s = openSession();
+            s.beginTransaction();
+            data = s.get(ReadWriteAccessDomainData.class, id);
+            log.info("===2 name: {} value: {}", data.getName(), data.value);
+            data.setName("updated");
+            s.flush();
+            s.getTransaction().commit();
+            s.clear();
+            s.close();
+            return data;
+        };
+
+        List<Callable<ReadWriteAccessDomainData>> callables = new ArrayList<>(parallel);
+
+        for (int i = 0; i < parallel / 2; i++) {
+            callables.add(incrementDataValue);
+        }
+
+        for (int i = 0; i < parallel / 2; i++) {
+            callables.add(setName);
+        }
+
+        scheduledExecutorService.invokeAll(callables);
+        Thread.sleep(1000L);
     }
 
     @Test
