74c45eb61e9c6e06aeb142dcd419d2eaa214499f
rica.rdo <rica.rdo@kakaopaycorp.com>
2021-04-16 18:51:58 +0900

    Add value field in ReadWriteAccessDomainData

--- src/test/java/hibernate/domain/ReadWriteAccessDomainData.java
+++ src/test/java/hibernate/domain/ReadWriteAccessDomainData.java
@@ -2,19 +2,23 @@ package hibernate.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-import org.hibernate.annotations.NaturalId;
-import org.hibernate.annotations.NaturalIdCache;
+import org.hibernate.annotations.*;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
-@NamedQueries(@NamedQuery(name = "domainDataNamedQuery", query = "from ReadWriteAccessDomainData where name = :name"))
+@NamedQueries(
+        {@NamedQuery(name = "domainDataNamedQuery", query = "from ReadWriteAccessDomainData where name = :name"),
+                @NamedQuery(name = "domainDataNamedQueryById", query = "from ReadWriteAccessDomainData where id = :id")}
+)
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE, region = ReadWriteAccessDomainData.CACHE_REGION_NAME)
 @NaturalIdCache
 @Data
 @NoArgsConstructor
+@DynamicUpdate
 public class ReadWriteAccessDomainData {
     public static final String CACHE_REGION_NAME = "DomainData";
     public static final String NATURAL_ID_CACHE_REGION_NAME = CACHE_REGION_NAME + "__NaturalId";
@@ -23,7 +27,9 @@ public class ReadWriteAccessDomainData {
     private Long id;
 
     private String name;
-    
+
+    public int value = 0;
+
     @NaturalId
     private long naturalId;
 
